4f2b87914d6c9be54ec33394fe6af4bc3f827a23
Petro.Diak <petro.diak@trinetix.com>
2018-06-12 18:30:21 +0300

    Add BookStore project

--- BookStoreMVC/BookStore/BookStore/Controllers/HomeController.cs
+++ BookStoreMVC/BookStore/BookStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,37 @@ namespace BookStore.Controllers
 {
   public class HomeController : Controller
   {
+    // создаем контекст данных
+    BookContext db = new BookContext();
+
     public ActionResult Index()
     {
+      // получаем из бд все объекты Book      
+      IEnumerable<Book> books = db.Books;
+      // передаем все объекты в динамическое свойство Books в ViewBag
+      ViewBag.Books = books;
+      // возвращаем представление
       return View();
     }
 
-    public ActionResult About()
-    {
-      ViewBag.Message = "Your application description page.";
 
+    [HttpGet]
+    public ActionResult Buy(int id)
+    {
+      ViewBag.BookId = id;
       return View();
     }
 
-    public ActionResult Contact()
+    [HttpPost]
+    public string Buy(Purchase purchase)
     {
-      ViewBag.Message = "Your contact page.";
-
-      return View();
+      purchase.Date = DateTime.Now;
+      // добавляем информацию о покупке в базу данных
+      db.Purchases.Add(purchase);
+      // сохраняем в бд все изменения
+      //принимает переданную ему в запросе POST модель purchase и добавляет ее в базу данных.
+      db.SaveChanges();
+      return "Спасибо," + purchase.Person + ", за покупку!";
     }
   }
 }
---
+++ BookStoreMVC/BookStore/BookStore/Models/BookDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookStore.Models
+{
+  //Класс DropCreateDatabaseAlways позволяет при каждом новом запуске заполнять базу данных заново некоторыми начальными данными.
+  public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+  {
+    protected override void Seed(BookContext db)
+    {
+      //Используя метод db.Books.Add мы добавляем каждый такой объект в базу данных.
+      db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+      db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Price = 180 });
+      db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+      db.Books.Add(new Book { Name = "Book4", Author = "Author4", Price = 222 });
+
+      base.Seed(db);
+    }
+  }
+}
