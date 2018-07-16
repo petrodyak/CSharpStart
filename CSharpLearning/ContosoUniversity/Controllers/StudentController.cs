08eefa3489ed6b39967f94a9b6eacb5be056391a
Petro.Diak <petro.diak@trinetix.com>
2018-07-16 19:16:26 +0300

    Add loging

--- CSharpLearning/ContosoUniversity/Controllers/StudentController.cs
+++ CSharpLearning/ContosoUniversity/Controllers/StudentController.cs
@@ -8,19 +8,36 @@ using System.Web;
 using System.Web.Mvc;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
+using PagedList;
+using System.Data.Entity.Infrastructure;
 
 namespace ContosoUniversity.Controllers
 {
-    public class StudentController : Controller
-    {
-        private SchoolContext db = new SchoolContext();
+  public class StudentController : Controller
+  {
+    private SchoolContext db = new SchoolContext();
 
-        // GET: Student
-        public ActionResult Index(string sortOrder, string searchString)
+    // GET: Student
+    public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
     {
+      ViewBag.CurrentSort = sortOrder;
       ViewBag.LastNameSortParm = String.IsNullOrEmpty(sortOrder) ? "lastName_desc" : "";
       ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
       ViewBag.FirstNameSortParm = sortOrder == "firstName" ? "firstName_desc" : "firstName";
+      ViewBag.EmailSortParm = sortOrder == "Email" ? "Email_desc" : "Email";
+      //If the search string is changed during paging, the page has to be reset to 1, because the new filter can result in different data to display. 
+      //The search string is changed when a value is entered in the text box and the submit button is pressed. In that case, the searchString parameter is not null.
+      if (searchString != null)
+      {
+        page = 1;
+      }
+      else
+      {
+        searchString = currentFilter;
+      }
+
+      ViewBag.CurrentFilter = searchString;
+
       var students = from s in db.Students
                      select s;
 
@@ -31,7 +48,10 @@ namespace ContosoUniversity.Controllers
 
       /*The query is not executed until you convert the IQueryable object into a collection by calling a method such as ToList. 
        *Therefore, this code results in a single query that is not executed until the return View statement.*/
-      return View(students.ToList());
+      int pageSize = 3;
+      //page ?? 1) means return the value of page if it has a value, or return 1 if page is null.
+      int pageNumber = (page ?? 1);
+      return View(students.ToPagedList(pageNumber, pageSize));
     }
 
     private static IQueryable<Student> StudentFilterBySearchString(string searchString, IQueryable<Student> students)
@@ -73,148 +93,148 @@ namespace ContosoUniversity.Controllers
 
     // GET: Student/Details/5
     public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
-
-        // GET: Student/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Student/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "LastName,FirstMidName,EnrollmentDate,EmailAddress")] Student student)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    db.Students.Add(student);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            }
-            catch (DataException)
-            {
-                //Log the error (uncomment dex variable name and add a line here to write a log.
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            }
-            return View(student);
-        }
-
-        // GET: Student/Edit/5
-        [HttpGet]
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
-
-        // POST: Student/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost, ActionName("Edit")]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit()
-        {
-            int? id = Int32.Parse(Request.Params["id"]);
-
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            var studentToUpdate = db.Students.Find(id);
-
-            //These changes implement a security best practice to prevent overposting, 
-            if (TryUpdateModel(studentToUpdate, "",
-             new string[] { "LastName", "FirstMidName", "EnrollmentDate", "EmailAddress" }))
-            {
-                try
-                {
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-                catch (DataException)
-                {
-
-                    //Log the error (uncomment dex variable name and add a line here to write a log.
-                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
-
-                }
-            }
-            return View(studentToUpdate);
-
-        }
-
-        // GET: Student/Delete/5
-        public ActionResult Delete(int? id, bool? saveChangesError = false)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            if (saveChangesError.GetValueOrDefault())
-            {
-                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
-
-        // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-
-            try
-            {
-                Student student = db.Students.Find(id);
-                db.Students.Remove(student);
-                db.SaveChanges();
-            }
-            catch (DataException/* dex */)
-            {
-                //Log the error (uncomment dex variable name and add a line here to write a log.
-                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
-            }
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Student student = db.Students.Find(id);
+      if (student == null)
+      {
+        return HttpNotFound();
+      }
+      return View(student);
     }
+
+    // GET: Student/Create
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: Student/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create([Bind(Include = "LastName,FirstMidName,EnrollmentDate,EmailAddress")] Student student)
+    {
+      try
+      {
+        if (ModelState.IsValid)
+        {
+          db.Students.Add(student);
+          db.SaveChanges();
+          return RedirectToAction("Index");
+        }
+      }
+      catch (RetryLimitExceededException )
+      {
+        //Log the error (uncomment dex variable name and add a line here to write a log.
+        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+      }
+      return View(student);
+    }
+
+    // GET: Student/Edit/5
+    [HttpGet]
+    public ActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Student student = db.Students.Find(id);
+      if (student == null)
+      {
+        return HttpNotFound();
+      }
+      return View(student);
+    }
+
+    // POST: Student/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost, ActionName("Edit")]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit()
+    {
+      int? id = Int32.Parse(Request.Params["id"]);
+
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+
+      var studentToUpdate = db.Students.Find(id);
+
+      //These changes implement a security best practice to prevent overposting, 
+      if (TryUpdateModel(studentToUpdate, "",
+       new string[] { "LastName", "FirstMidName", "EnrollmentDate", "EmailAddress" }))
+      {
+        try
+        {
+          db.SaveChanges();
+          return RedirectToAction("Index");
+        }
+        catch (RetryLimitExceededException )
+        {
+
+          //Log the error (uncomment dex variable name and add a line here to write a log.
+          ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+
+        }
+      }
+      return View(studentToUpdate);
+
+    }
+
+    // GET: Student/Delete/5
+    public ActionResult Delete(int? id, bool? saveChangesError = false)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      if (saveChangesError.GetValueOrDefault())
+      {
+        ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+      }
+      Student student = db.Students.Find(id);
+      if (student == null)
+      {
+        return HttpNotFound();
+      }
+      return View(student);
+    }
+
+    // POST: Student/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+
+      try
+      {
+        Student student = db.Students.Find(id);
+        db.Students.Remove(student);
+        db.SaveChanges();
+      }
+      catch (RetryLimitExceededException /* dex */)
+      {
+        //Log the error (uncomment dex variable name and add a line here to write a log.
+        return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+      }
+      return RedirectToAction("Index");
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+  }
 }
--- CSharpLearning/ContosoUniversity/Global.asax.cs
+++ CSharpLearning/ContosoUniversity/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ContosoUniversity.DAL;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace ContosoUniversity
 {
@@ -16,6 +18,10 @@ namespace ContosoUniversity
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
+      //These lines of code are what causes your interceptor code
+      //to be run when Entity Framework sends queries to the database.
+      DbInterception.Add(new SchoolInterceptorTransientErrors());
+      DbInterception.Add(new SchoolInterceptorLogging());
     }
   }
 }
