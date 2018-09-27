2d1d171163f236dd5314710ee821be475c006b7d
Petro.Diak <petro.diak@trinetix.com>
2018-09-27 14:56:53 +0300

    Add RowVersion column to Department entity

--- CSharpLearning/ContosoUniversity/Models/Department.cs
+++ CSharpLearning/ContosoUniversity/Models/Department.cs
@@ -5,28 +5,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-  public class Department
-  {
-    public int DepartmentID { get; set; }
+    public class Department
+    {
+        public int DepartmentID { get; set; }
 
-    [StringLength(50, MinimumLength = 3)]
-    public string Name { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
 
-    [DataType(DataType.Currency), 
-    //Set destination column type money
-    Column(TypeName = "money")]
-    public decimal Budget { get; set; }
+        [DataType(DataType.Currency),
+        //Set destination column type money
+        Column(TypeName = "money")]
+        public decimal Budget { get; set; }
 
-    [DataType(DataType.Date), Display(Name = "Start Date")]
-    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-    public DateTime StartDate { get; set; }
+        [DataType(DataType.Date), Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
 
-    //int? Can be null
-    public int? InstructorID { get; set; }
+        //int? Can be null
+        public int? InstructorID { get; set; }
 
-    public virtual Instructor Administrator { get; set; }
-    public virtual ICollection<Course> Courses { get; set; }
+        //The Timestamp attribute specifies that this column will be included 
+        //in the Where clause of Update and Delete commands sent to the database
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        public virtual Instructor Administrator { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
 
 
-  }
+    }
 }
