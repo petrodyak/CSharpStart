98b2cd44d4cb761133f7f72b4dfc190d8a6d77a7
Petro.Diak <petro.diak@trinetix.com>
2019-04-26 15:56:53 +0300

    BookStore . First Commit

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Create Context of data
+        //Затем создается объект контекста данных, через который мы будем взаимодействовать с бд
+        BookContext db = new BookContext(); 
+
+        public ActionResult Index()
+        {
+            //get from DB all Book Objects
+            IEnumerable<Book> books = db.Books;
+
+            //Send all objects inot dymanic property Books in ViewBag
+            //Для передачи списка объектов Book в представление используем объект ViewBag.
+            ViewBag.Books = books;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/BookDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookStore.Models
+{
+    //Класс DropCreateDatabaseAlways позволяет при каждом новом запуске заполнять базу данных заново некоторыми начальными данными.
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Book1", Author = "Author1", Price = 110 });
+            db.Books.Add(new Book { Name = "Book2", Author = "Author2", Price = 220 });
+            db.Books.Add(new Book { Name = "Book3", Author = "Author3", Price = 330 });
+
+            base.Seed(db);
+        }
+    }
+}
